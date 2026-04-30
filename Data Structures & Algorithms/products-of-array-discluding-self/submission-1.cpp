3968d44cb0751c51e1c5ad42fdca671c03215314
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-04-30 14:06:12 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // Prefix & Suffix 
+    // Time complexity: O(n)
+    vector<int> productExceptSelf(vector<int>& nums) {
+      std::vector<int> prod(nums.size(),1);
+      int p = 1;
+      for(int i=0;i<nums.size();i++){
+        prod[i] *= p;
+        p *= nums[i]; 
+      }
+
+      p = 1;
+      for(int i=nums.size()-1;i>=0;i--){
+        prod[i] *= p;
+        p *= nums[i];
+      }
+      return prod;
+    }
+};
