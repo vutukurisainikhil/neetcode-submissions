b3ace026b18e2427bcf55905cabf4ee8e7671e8d
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-04 09:59:18 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<pair<int, int>> time;
+        for (const auto& i : intervals) {
+            time.push_back({i.start, 1});
+            time.push_back({i.end, -1});
+        }
+
+        sort(time.begin(), time.end(), [](auto& a, auto& b) {
+            return a.first == b.first ? a.second < b.second : a.first < b.first;
+        });
+
+        int res = 0, count = 0;
+        for (const auto& t : time) {
+            // meeting rooms get added when overlap
+            // meeting rooms get deleted as meeting ends.
+            count += t.second;
+            res = max(res, count);
+        }
+        return res;
+    }
+};
