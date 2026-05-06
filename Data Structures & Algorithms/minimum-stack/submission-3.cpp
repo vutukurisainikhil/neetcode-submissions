99355587a864eb86129229b8900a9e277137f987
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-06 19:27:26 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,39 @@
+class MinStack {
+public:
+    // key, min_key
+    vector<pair<int,int>> v;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(v.size()==0){
+            v.push_back({val, val});
+        }
+        else{
+            auto p = v.back();
+            v.push_back({val, min(p.second, val)});
+        }
+    }
+    
+    void pop() {
+        v.pop_back();
+    }
+    
+    int top() {
+        return v.back().first;
+    }
+    
+    int getMin() {
+        return v.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
