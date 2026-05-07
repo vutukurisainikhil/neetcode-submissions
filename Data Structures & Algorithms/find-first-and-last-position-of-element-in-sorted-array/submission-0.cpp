54b732cb285961af9005ebe4c06e25cada861f2e
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-07 20:24:54 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int start = binarySearch(nums, target, n);
+        if (start == n || nums[start] != target) {
+            return {-1, -1};
+        }
+
+        return {start, binarySearch(nums, target + 1, n) - 1};
+    }
+
+private:
+    int binarySearch(vector<int>& nums, int target, int n) {
+        int l = 0, r = n;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+};
