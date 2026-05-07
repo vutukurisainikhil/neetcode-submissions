267a556d1a78ae3b51336f0a28e11c85689f780d
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-07 18:44:43 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) {
+        combinations(nums, 0, nums.size()-1);
+        return res;
+    }
+     void combinations(vector<int> &nums, int index, int end){
+        if(index==end){
+            res.push_back(nums);
+        }
+        for(int i=index;i<=end;i++){
+            swap(nums[index], nums[i]);
+            combinations(nums, index+1, end);
+            swap(nums[index], nums[i]);
+        }
+    }
+};
