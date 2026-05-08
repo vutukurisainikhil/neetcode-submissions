1f1ed67cb20ea2c606b97c9df087fd64cfcb159c
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-08 14:11:25 +0530

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /* Using Dynamic programming with AND logic in between cells is the key.
+       For explanation check in solution
+    */
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        vector<bool> dp(s2.length() + 1);
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    // if i=0 then j cant be zero, so use j-1.
+                    dp[j] = dp[j - 1] && s2[j - 1] == s3[i + j - 1];
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1[i - 1] == s3[i + j - 1];
+                } else {
+                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) ||
+                            (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                }
+            }
+        }
+        return dp[s2.length()];
+    }
+};
