f7f97bae31aed61fd49e030ae8f1ae28f9657200
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-08 15:18:46 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0, lowest_price = prices[0];
+        for(int i=1;i<prices.size();i++){
+            lowest_price = min(lowest_price, prices[i]);
+            max_profit = max(max_profit, (prices[i]-lowest_price));
+        }
+        return max_profit;
+    }
+};
