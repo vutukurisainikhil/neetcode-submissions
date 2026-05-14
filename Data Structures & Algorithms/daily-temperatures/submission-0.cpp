56b98a5b5cc446568f2bde9e60fe61b110425b5d
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-14 22:12:05 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res;
+        stack<int> st;
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            auto temp = temperatures[i];
+            // Remove the stack elements if temp[st.top()] <= curr temp.
+            while (!st.empty() && temperatures[st.top()] <= temp) {
+                st.pop();
+            }
+            (st.empty()) ? res.push_back(0) : res.push_back(st.top() - i);
+            st.push(i);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
