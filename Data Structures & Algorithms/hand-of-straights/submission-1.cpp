a00b90b602b8198757c26316daeee7f5fbb9c8ed
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-15 15:17:59 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) 
+            return false;
+
+        unordered_map<int, int> count;
+        for (int num : hand) 
+            count[num]++; // freq map
+
+        sort(hand.begin(), hand.end());
+        for (int num : hand) {
+            if (count[num] > 0) {
+                for (int i = num; i < num + groupSize; i++) {
+                    if (count[i] == 0) 
+                        return false; // not consecutive
+                    count[i]--;
+                }
+            }
+        }
+        return true;
+    }
+};
