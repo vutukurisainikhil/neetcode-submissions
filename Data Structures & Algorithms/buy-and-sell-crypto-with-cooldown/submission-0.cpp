529733f1ef945198c8bc813cae284b6b5150e445
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-15 14:01:23 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // TimeComplexity - O(N)
+    // Approach =  bottom-up DP
+    int maxProfit(vector<int>& prices) {
+        
+        // initialization
+        int coolDown=0, sell=0, hold=INT_MIN;
+        
+        for(int stockPrice_Day_i : prices){
+            
+            int prevCoolDown = coolDown, prevSell = sell, prevHold = hold;
+            
+            // Max profit of cooldown on Day i comes from either cool down of Day_i-1, 
+            // OR sell out of Day_i-1 and today Day_i is cooling day
+            coolDown = max(prevCoolDown, prevSell);
+            
+            // Max profit of sell on Day_i comes from hold of Day_i-1 and sell on Day_i
+            sell = prevHold + stockPrice_Day_i;
+
+            // Max profit of hold on Day_i comes from either hold of Day_i-1, 
+            // or cool down on Day_i-1 and buy on Day_i
+            hold = max(prevHold, prevCoolDown - stockPrice_Day_i);   
+        }
+        // The state of max profit must be either sell or cool down
+        return max( sell, coolDown );
+    }
+};
