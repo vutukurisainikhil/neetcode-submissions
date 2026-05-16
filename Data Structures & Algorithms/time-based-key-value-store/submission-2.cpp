f9217ace62a762562ff1409afddc07d0c2594c21
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-16 17:01:57 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,31 @@
+class TimeMap {
+private:
+    // TimeComplexity: O(1) for set & O(logN) for get with binary search
+    unordered_map<string, vector<pair<int, string>>> keyStore;
+
+public:
+    TimeMap() {}
+
+    void set(string key, string value, int timestamp) {
+        keyStore[key].emplace_back(timestamp, value);
+    }
+
+    string get(string key, int timestamp) {
+        auto& values = keyStore[key];
+        // we can inset in increased timestamp, we can use binary search.
+        int left = 0, right = values.size() - 1;
+        string result = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (values[mid].first <= timestamp) {
+                result = values[mid].second;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
