529f7db89cd3a734c17be5d1fd2019aa503c41e3
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-16 10:37:27 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned long long> dp(amount + 1, 0);
+        dp[0] = 1;
+        // for combinations of coins, outer-loop is coins.
+        // for each coin, can we gwt to target ?.
+        for (auto c : coins) {
+            for (int j = c; j <= amount; j++) {
+                dp[j] += dp[j - c];
+            }
+        }
+        return dp[amount];
+    }
+};
