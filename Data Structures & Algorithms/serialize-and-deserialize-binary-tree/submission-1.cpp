45177d089d3dcc3b9ffd12b9defd5681ea5baf94
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-16 09:37:41 +0530

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // Encodes a tree to a single string of preorder traversal.
+    // queue should be used to decode with NULL logic, in preorder building the
+    // tree back
+
+    string serialize(TreeNode* root) {
+        if (root == NULL)
+            return "null"; // NULL wiil help to decode this logic
+        string s;
+        s += to_string(root->val);
+        string serial_left = serialize(root->left);
+        string serial_right = serialize(root->right);
+        return s + "," + serial_left + "," + serial_right;
+    }
+
+    TreeNode* helper_deserialize(queue<string>& q) {
+        if (q.empty())
+            return NULL;
+        string s = q.front();
+        q.pop();
+        if (s == "null")
+            return NULL; // stops adding on left/right.
+        int num = stoi(s);
+        TreeNode* node = new TreeNode(num);
+        node->left = helper_deserialize(q);
+        node->right = helper_deserialize(q);
+        return node;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        queue<string> q; // queue should be used
+        string temp;
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i] == ',') {
+                q.push(temp);
+                temp = "";
+            } else
+                temp.push_back(data[i]);
+        }
+        return helper_deserialize(q);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
