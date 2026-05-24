1131ac0035663914c8071108809ec45d9adabd7d
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-05-24 07:38:48 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::map<int,int> m;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            if(m[t[i]]<=0){
+                return false;
+            }
+            m[t[i]]--;
+        }
+        return true;
+    }
+};
