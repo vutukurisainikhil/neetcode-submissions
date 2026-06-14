0fb438ae0ea9194fc3c922b4767ab5be3364ba12
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-06-14 10:05:33 +0530

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    // TimeComplexity - O(nlogn).
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& x, auto& y) {
+            return x.start < y.start;
+        });
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
