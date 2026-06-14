e450d6434822160306b09cd0343850da508b21e0
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-06-14 08:04:07 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // Time Complexity: O(n)
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        unordered_set<int> good;
+
+        for (const auto& t : triplets) {
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+            for (int i = 0; i < t.size(); i++) {
+                // for each exact position
+                if (t[i] == target[i]) {
+                    good.insert(i);
+                }
+            }
+        }
+        return good.size() == 3;
+    }
+};
