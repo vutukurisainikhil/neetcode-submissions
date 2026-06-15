88c1740c0079caf241ea22b7797ffba3eb49a302
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-06-15 09:36:47 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    // Time complexity: O(n⋅m⋅k)
+    // Space complexity:O(n)
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                // Here, substring not including i. so till j to i-1 only.
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
