e6ea3882eb9099165f14b48da5a3674e734d71e9
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-06-15 11:58:17 +0530

    Add: word-ladder - submission-5

---
+++ Data Structures & Algorithms/word-ladder/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    // TimeComplexity: O(N*L^2)
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> set(wordList.begin(), wordList.end());
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});  // start
+
+        while (!q.empty()) {
+            pair<string, int> curr = q.front();
+            q.pop();
+            int steps = curr.second;
+            string word = curr.first;
+            if (word == endWord)
+                return steps;  // early return would be shortest transformation seq.
+            for (int i = 0; i < word.length(); i++) {
+                char org = word[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    word[i] = c;
+                    if (set.find(word) != set.end()) {
+                        q.push({word, steps + 1});
+                        set.erase(word); // erase as visited
+                    }
+                }
+                word[i] = org;
+            }
+        }
+        return 0;
+    }
+};
