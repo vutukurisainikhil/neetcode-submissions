ca3ad5056ee87695b78ab3166d37cf3a6650efa3
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-08-15 07:29:39 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0, j = s.size()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])){
+                cout<<"s[i]: "<<s[i]<<" s[j]:"<<s[j]<<endl;
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
