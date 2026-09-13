2bbc66d83e1c782ee617099ee8adda8d16bbbb50
vutukurisainikhil <53534990+vutukurisainikhil@users.noreply.github.com>
2026-09-13 12:29:04 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    // Time complexity: O(2^(t/n)), where t is target, n is nums size().
+    // Space Complexity: O(t/n)
+    vector<vector<int>> res;
+   public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> curr;
+        dfs(0, nums, target, 0, curr);
+        return res;
+    }
+
+    void dfs(int index, vector<int>& nums, int target, int sum, vector<int>& curr) {
+        if (target == sum) {
+            res.push_back(curr);
+            return;
+        }
+
+        for (int i = index; i < nums.size(); i++) {
+            if (sum + nums[i] > target) {
+                return;
+            }
+            curr.push_back(nums[i]);
+            dfs(i, nums, target, sum + nums[i], curr);
+            curr.pop_back();
+        }
+    }
+};
